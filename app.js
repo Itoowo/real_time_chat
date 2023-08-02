0607dc1d19b086a23f0a007a11b3a7a2cfe6ca6d
Itoowo <kuragari.ito@gmail.com>
2023-08-02 22:05:50 +0700

    add some new functions

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const indexRoute = require('./routes/indexRoute')
+const indexRoute = require('./routes/indexRoute');
+const fileUpload = require('express-fileupload');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(fileUpload());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
--- controllers/indexCon.js
+++ controllers/indexCon.js
@@ -1,29 +1,56 @@
-const homepage = (req, res)=>{
-    res.render('homepage', {title: 'Disnut - Welcome'});
+const fs = require('fs');
+const con = require('../config/db');
+
+const index = (req, res)=>{
+    res.render('/')
 }
 
-const signup = (req, res)=>{
-    res.render('signup', {title: 'Disnut - Sign Up'})
+const homepage = (req, res) => {
+    res.render('homepage', { title: 'Disnut - Welcome' });
 }
 
-const signup_post = (req, res)=>{
+const signup = (req, res) => {
+    res.render('signup', { title: 'Disnut - Sign Up' })
+}
+
+const signup_post = (req, res) => {
+    if (req.files) {
+        let timestamp = Date.now();
+        let file = req.files.file;
+        let filename = timestamp + file.name;
+        console.log(file)
+        file.mv('./public/uploads/' + filename, (err) => {
+            if (err) throw err;
+        })
+        img = filename;
+    }
     let body = req.body;
     let first_name = body.firstname;
     let last_name = body.lastname;
     let fname = first_name + '' + last_name;
-    let myArr = [fname, body.password];
-
+    let myArr = [fname, body.password, img];
+    let sql = 'insert into `user` (`fname`, `password`, `img`) values(?,?,?)';
+    con.query(sql, myArr, (err, result) => {
+        if (err) throw err;
+        else {
+            res.redirect('/homepage')
+        }
+    })
 }
 
-const login = (req, res)=>{
-    res.render('login', {title: 'Disnut - Login'});
+const login = (req, res) => {
+    res.render('login', { title: 'Disnut - Login' });
 }
 
-
+const login_post = (req, res)=>{
+    console.log(req.body)
+}
 
 module.exports = {
+    index,
     login,
     homepage,
     signup,
     signup_post,
+    login_post
 }
